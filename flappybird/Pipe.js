9b08dd765ecdaecc0a43c5fd05be130bd9e94638
MishraMukesh117 <Mukesh.Mishra@accoliteindia.com>
2020-05-01 20:00:30 +0530

    source code for flappy-bird game

---
+++ flappybird/Pipe.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import {View, Image} from 'react-native';
+import Images from './assets/Images';
+
+export default class Pipe extends Component {
+    render() {
+        const width = this.props.body.bounds.max.x - this.props.body.bounds.min.x;
+        const height = this.props.body.bounds.max.y - this.props.body.bounds.min.y;
+         const x = this.props.body.position.x -width/2;
+         const y = this.props.body.position.y - height/2;
+          const pipeRatio = 160/width;
+          const pipeHeight = 30 * pipeRatio;
+          const pipeIteration = Math.ceil(height/pipeHeight);
+        return (
+            <View style={{
+                position: 'absolute',
+                top: y,
+                left:x,
+                 width: width,
+                height: height,
+                overflow: 'hidden',
+                flexDirection: 'column'
+            }}>
+                
+                {Array.apply(null, Array(pipeIteration)).map((el, index) => {
+                    return <Image style={{ width: width, height: pipeHeight }} key={index} resizemode='stretch' source={Images.pipeCore} />
+                })}
+            </View>
+        )
+    }
+}
